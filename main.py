345d9f916b41a8d849cfc1b037a8a51618618ded
AinerPaker <121055371+AinerPaker@users.noreply.github.com>
2023-03-28 18:02:58 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-liczby = input("podaj liczby (oddziel przecinkiem): ")
-tab = liczby.split(',')
-min = int(tab[0])
-max = int(tab[0])
-for tab in tab:
-    if(int(tab)<min): min = int(tab)
-    if(int(tab)>max): max = int(tab)
-print("Najmniejsza: " + str(min) + "\nNajwieszka: " + str(max))
